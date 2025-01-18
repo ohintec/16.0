d851e502fe70fc8668051f0dfe2c10a7905b57ad
OHint <info@ohint.net>
2025-01-18 15:24:46 +0200

    all images

--- n2d_logo_full_header/__manifest__.py
+++ n2d_logo_full_header/__manifest__.py
@@ -25,7 +25,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/report_layout_inherit.xml',
-        'static/description/index.html',
+        'static/src/templates.xml',
     ],
    'assets': {
         'web.assets_backend': [
