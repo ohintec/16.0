6be57265e30f152f27279b33e6d1c88948a3feb3
OHint <info@ohint.net>
2025-01-18 13:54:46 +0200

    logo

--- n2d_logo_full_header/__manifest__.py
+++ n2d_logo_full_header/__manifest__.py
@@ -26,7 +26,7 @@
         'views/views.xml',
         'views/report_layout_inherit.xml',
     ],
-    'images': ['static/description/cover_image.png'],
+    'images': ['static/description/cover_image.png', 'static/description/Hintegration.png','static/description/invoice.png ', 'static/description/bannar.jpeg'],
 
     'price': '5.00',
 
