dcf9a5b16a4ef89ca50c7a506e3cdf2cf91fc5fe
OHint <info@ohint.net>
2025-01-19 11:03:58 +0200

    logo

--- n2d_logo_full_header/__manifest__.py
+++ n2d_logo_full_header/__manifest__.py
@@ -9,7 +9,7 @@
         N2d Logo Full Header
     """,
 
-    'author': "Hintegration",
+    'author': "Hitegration",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
